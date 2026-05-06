b457c084e6b2c60a9b799dd527cc56f68f647c72
bidfastr-gif <bidfastr@gamil.com>
2026-05-06 11:55:43 +0530

    Proper Fix: Component naming alignment and structural cleanup

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -8,34 +8,34 @@ import { WhyChooseUs } from "@/components/WhyChooseUs";
 import { Footer } from "@/components/Footer";
 import pattern from "@/assets/food-pattern.jpg";
 
-const Home = () => {
-    return (
-        <div className="min-h-screen bg-background">
-            <Header />
-            <main className="relative overflow-hidden">
-                {/* Background Pattern */}
-                <div className="absolute inset-0 opacity-[0.08] pointer-events-none">
-                    <div
-                        className="absolute inset-0"
-                        style={{
-                            backgroundImage: `url(${pattern})`,
-                            backgroundSize: "auto",
-                            backgroundRepeat: "repeat",
-                        }}
-                    />
-                </div>
-                <div className="relative z-10">
-                    <Hero />
-                    <EnquirySection />
-                    <Categories />
-                    <AboutSection />
-                    <StatisticsBanner />
-                    <WhyChooseUs />
-                </div>
-            </main>
-            <Footer />
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-background">
+      <Header />
+      <main className="relative overflow-hidden">
+        {/* Background Pattern */}
+        <div className="absolute inset-0 opacity-[0.08] pointer-events-none">
+          <div
+            className="absolute inset-0"
+            style={{
+              backgroundImage: `url(${pattern})`,
+              backgroundSize: "auto",
+              backgroundRepeat: "repeat",
+            }}
+          />
         </div>
-    );
+        <div className="relative z-10">
+          <Hero />
+          <EnquirySection />
+          <Categories />
+          <AboutSection />
+          <StatisticsBanner />
+          <WhyChooseUs />
+        </div>
+      </main>
+      <Footer />
+    </div>
+  );
 };
 
-export default Home;
+export default Index;
